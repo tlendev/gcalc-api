ba24441cc1065226b91bfac1142fefa5620177aa
tlendev <oxyaego98@gmail.com>
2022-07-09 22:35:55 +0200

    Add cors and helmet

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@
  * Big thank you to honeyhunterworld for providing the api data
  * https://genshin.honeyhunterworld.com/
  */
-
 import express from 'express';
 import { appRouter } from './app';
 import { charactersRouter } from './character/index';
+import helmet from 'helmet';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8080;
 
 async function bootstrap() {
     const app = express();
 
+    app.use(helmet());
+    app.use(cors({ origin: process.env.ALLOW_URL }));
     app.use('/', appRouter);
     app.use('/characters', charactersRouter);
 
