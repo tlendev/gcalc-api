eb106af107580f8505d4ca66e0688337da1d6db1
tlendev <oxyaego98@gmail.com>
2022-07-11 18:28:10 +0200

    Better not found handling

--- src/img/index.ts
+++ src/img/index.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express';
+import { PathLike } from 'fs';
+import { access } from 'fs/promises';
 import { join } from 'path';
+
+const exists = async (path: PathLike) => {
+    try {
+        await access(path);
+        return true;
+    } catch {
+        return false;
+    }
+};
 
 const imgRouter = Router();
 
-imgRouter.get('/:file', (req, res) => {
+imgRouter.get('/:file', async (req, res) => {
     const { file } = req.params;
+    if (!(await exists(join(process.cwd(), 'collection', 'media', file)))) {
+        res.status(404).json({ message: 'File not found' }).end();
+        return;
+    }
     res.sendFile(join(process.cwd(), 'collection', 'media', file));
 });
 
